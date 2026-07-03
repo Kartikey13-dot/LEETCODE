f9ad381771fb5975bd7c48230568f2dc9e968754
Kartikey13-dot <gairolakartikey6@gmail.com>
2026-07-03 13:12:00 +0530

    Time: 0 ms (100%), Space: 20.7 MB (53.94%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n=nums.size();
+        int left=0;
+        for(int right=0;right<n;right++)
+        {
+            if(nums[right]%2==0)
+            {
+                swap(nums[left],nums[right]);
+                left++;
+            }
+        }
+        return nums;
+    }
+};
