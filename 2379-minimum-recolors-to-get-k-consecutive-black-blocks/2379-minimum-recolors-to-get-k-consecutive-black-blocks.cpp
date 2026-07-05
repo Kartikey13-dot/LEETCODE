560b92f071a15250bbcdf22abcb20f134d5ef89d
Kartikey13-dot <gairolakartikey6@gmail.com>
2026-07-05 19:51:55 +0530

    Time: 0 ms (100%), Space: 8.6 MB (3.73%) - LeetHub

---
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int low=0;
+        int n=blocks.size();
+        int high=k-1;
+        int res=INT_MAX;
+        int ans=0;
+        unordered_map<char,int> mp;
+        for(int i=low;i<=high;i++)
+        {
+           if(blocks[i]=='W')
+           {
+            mp['W']++;
+            ans=mp['W'];
+           }
+        }
+        while(high<n)
+        {
+          res=min(ans,res);
+          if(blocks[low]=='W')
+          mp['W']--;
+          low++;
+          high++;
+          if(high==n)
+          break;
+          if(blocks[high]=='W')
+          mp['W']++;
+          ans=mp['W'];
+        }
+        return res;
+    }
+};
