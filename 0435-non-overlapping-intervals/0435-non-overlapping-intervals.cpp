f9d97f67fd0d12031d6a63fac8bd5c9ef085db97
Kartikey13-dot <gairolakartikey6@gmail.com>
2026-07-23 20:02:25 +0530

    Time: 121 ms (11.4%), Space: 107.2 MB (11.03%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -3,10 +3,9 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
        sort(intervals.begin(),intervals.end());
         int n=intervals.size();
-        int s1=intervals[0][0];
-        int e1=intervals[0][1];
+     
         vector<vector<int>> mp;
-        mp.push_back({s1,e1});
+        mp.push_back(intervals[0]);
         for(int i=1;i<n;i++)
         {
              int s2=intervals[i][0];
