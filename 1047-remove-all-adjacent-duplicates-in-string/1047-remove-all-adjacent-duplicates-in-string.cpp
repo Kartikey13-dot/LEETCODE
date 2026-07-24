7875217665b170bb34a0ee2c76fef52cb460fd3a
Kartikey13-dot <gairolakartikey6@gmail.com>
2026-07-24 22:42:13 +0530

    Time: 782 ms (5.05%), Space: 783.7 MB (5.09%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int n=s.size();
+        stack<char> st;
+        string d="";
+        for(int i=0;i<n;i++)
+        {
+            if(st.empty())
+            {st.push(s[i]);
+            continue;}
+            if(s[i]==st.top())
+           { st.pop();
+            continue;}
+            else
+            st.push(s[i]);
+        }
+        while(!st.empty())
+        {
+            char a=st.top();
+            d=a+d;
+            st.pop();
+        }
+         return d;
+    }
+};
